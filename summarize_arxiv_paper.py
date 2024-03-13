d6b379b044e7c974f43e658003e168bd56884ac9
Alok Abhishek <alok.abhishek@gmail.com>
2024-03-13 00:47:56 -0700

    more research reports and their summaries

--- summarize_arxiv_paper.py
+++ summarize_arxiv_paper.py
@@ -16,13 +16,11 @@ def summarize_local_pdf_using_openai(input_file, output_file, pages_to_exclude):
 
 base_input_path = "paper_pdf_repo/research_papers/"
 base_output_path = "paper_pdf_repo/summaries/"
-input_filename = "LocMoE_A_Low_overhead_MoE_for_Large_Language_Model_Training.pdf"
-output_filename = (
-    "LocMoE_A_Low_overhead_MoE_for_Large_Language_Model_Training_Summary.pdf"
-)
+input_filename = "A_Survey_on_Evaluation_of_Large_Language_Models.pdf"
+output_filename = "A_Survey_on_Evaluation_of_Large_Language_Models_Summary.pdf"
 input_file = os.path.join(base_input_path, input_filename)
 output_file = os.path.join(base_output_path, output_filename)
-pages_to_exclude = []
+pages_to_exclude = [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
 
 
 summarize_local_pdf_using_openai(input_file, output_file, pages_to_exclude)
--- utils/transformers/openai_summarizer.py
+++ utils/transformers/openai_summarizer.py
@@ -9,7 +9,7 @@ import re
 
 dotenv.load_dotenv()
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY2")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 
